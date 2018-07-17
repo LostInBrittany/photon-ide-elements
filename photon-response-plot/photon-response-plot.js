9e2ba715996b0b6041cfd37ddac67d116501f1ec
Horacio Gonzalez <horacio.gonzalez@gmail.com>
2018-07-17 17:27:45 +0200

    Now with C3

--- photon-response-plot/photon-response-plot.js
+++ photon-response-plot/photon-response-plot.js
@@ -1,6 +1,7 @@
 import { LitElement, html } from '@polymer/lit-element';
 import photonSharedStyles from '../photon-shared-styles.js';
 import timeseriesTools from '@photon-elements/photon-tools/photon-timeseries-tools';
+import '@granite-elements/granite-c3/granite-c3';
 
 class PhotonResponsePlot extends LitElement {
   _render({ stack, plottedPaths }) {
@@ -10,6 +11,7 @@ class PhotonResponsePlot extends LitElement {
     console.log('[photon-response-plot] - _render', this._dataFromPlottedTs(), stack, plottedPaths);
     return html`
         ${photonSharedStyles} 
+        ${this._renderC3()}
         ${Object.entries(this._dataFromPlottedTs()).map((tsByLevel) => html`
           ${tsByLevel[1].length >0 ?
             html`
@@ -75,7 +77,9 @@ class PhotonResponsePlot extends LitElement {
     let plottedData = {};
     Object.entries(this.plottedPaths).forEach((pathsByLevel) => {
       plottedData[pathsByLevel[0]] = pathsByLevel[1].map((path) => {
-        return this._queryObj(this.stack[pathsByLevel[0]], path);
+        let item = this._queryObj(this.stack[pathsByLevel[0]], path);
+        item.id=path;
+        return item;
       });
     });
     console.log('rendering', this.plottedPaths, this.stack, plottedData);
@@ -94,6 +98,60 @@ class PhotonResponsePlot extends LitElement {
       return (accumulator[current] !== undefined) ? accumulator[current] : false;
     }, obj);
     return data;
+  }
+
+  _chartConf() {
+    let axis = {
+      x: {
+        show: true,
+        centered: true,
+        type: 'timeseries',
+        tick: {
+          centered: true,
+          count: 6,
+          outer: false,
+          culling: { max: 4 },
+          fit: true,
+          multiline: true,
+          format: '%Y-%m-%d %H:%M:%S',
+        },
+      },
+      y: {
+        show: true,
+      },
+    };
+    return {
+      size: {
+        height: 340,
+      },
+      axis: axis,
+      legend: {
+        hide: false,
+      },
+      tooltip: {
+        grouped: true,
+      },
+    };
+  }
+
+  _renderC3() {
+    console.log('[photon-response-plot] _renderC3 - this._dataFromPlottedTs()', this._dataFromPlottedTs());
+    let stack = Object.entries(this._dataFromPlottedTs()).
+        map((entry) => entry[1]).
+        reduce((acc, val) => acc.concat(val), []);
+    console.log('[photon-response-plot] _renderC3 - stack', stack);
+  
+    let c3Data = timeseriesTools.timeseriesToC3(stack);
+    console.log('[photon-response-plot] _renderC3 - c3Data', c3Data);
+    
+    if (c3Data.columns && c3Data.columns.length >0) {
+      return html`
+        <granite-c3 
+            data=${c3Data} 
+            options=${this._chartConf()} 
+            debug></granite-c3>
+      `;
+    }
   }
 }
 
